3fdea2eb814a28ac4cd7eed69b73f253d146249d
HenryGye <henryedua@hotmail.com>
2023-08-24 21:30:32 -0500

    Ajuste componente instalacion

--- src/app/modules/instalacion/instalacion.component.ts
+++ src/app/modules/instalacion/instalacion.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 import { SharedService } from '../../shared/shared.service';
 
 @Component({
@@ -7,11 +8,28 @@ import { SharedService } from '../../shared/shared.service';
   styleUrls: ['./instalacion.component.css']
 })
 export class InstalacionComponent {
-  constructor(private sharedService: SharedService) {
+  panelInstalacionServicio: boolean = true;
+  panelRegistroCompleto: boolean = false;
+
+  constructor(
+    private sharedService: SharedService,
+    private routerparams: Router) {
     this.sharedService.setTimeLineCobertura(true);
     this.sharedService.setTimeLineDatosPersonales(true);
     this.sharedService.setTimeLineBiometriaFacial(true);
     this.sharedService.setTimeLineInstalacion(true);
     this.sharedService.setTimeLineActivo4(true);
+  }
+
+  continuar() {
+    // si todo ok
+    this.panelInstalacionServicio = false;
+    this.panelRegistroCompleto = true;
+  }
+
+  regresarInicio() {
+    this.routerparams.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.routerparams.navigate(['compra-en-linea/cobertura']);
+    });
   }
 }
