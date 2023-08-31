f0549d0d942a1cf6635172c8e3e2ddf32345b66c
HenryGye <henryedua@hotmail.com>
2023-08-31 10:29:53 -0500

    Ajuste componente resumen pedido

--- src/app/shared/resumen-pedido/resumen-pedido.component.ts
+++ src/app/shared/resumen-pedido/resumen-pedido.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./resumen-pedido.component.css']
 })
 export class ResumenPedidoComponent {
+  dataResumen: any = {
+    titulo: 'Resumen del pedido',
+    titulo2: 'Super Fibra Óptica',
+    precioEntero: '$22',
+    precioDecimal: '.31',
+    periodicidad: '/mes',
+    detalle1: 'Upgrade a 200 megas por siempre',
+    detalle2: 'Instalación sin costo',
+    detalle3: 'Equipo WIFI sin costo',
+    detalle4: 'Xtrim Play gratis',
+    detalle5: 'PERMANENCIA DE 24 MESES'
+  };
 
 }
