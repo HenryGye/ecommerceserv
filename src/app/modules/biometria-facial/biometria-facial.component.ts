060c6f2931b54eed53f5e852fecc311c5695b6c3
HenryGye <henryedua@hotmail.com>
2023-09-06 19:57:46 -0500

    Agregar validacion y redireccion biometria facial

--- src/app/modules/biometria-facial/biometria-facial.component.ts
+++ src/app/modules/biometria-facial/biometria-facial.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { NavigationExtras, Router } from '@angular/router';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
 import { SharedService } from '../../shared/shared.service';
 
 @Component({
@@ -21,9 +21,13 @@ export class BiometriaFacialComponent implements OnInit {
   spinner: boolean = false;
   showIconValidacionExistosa: boolean = false;
 
+  urlValidacionBiometriaFacial: string = '';
+  respuestaBiometria: any | undefined;
+
   constructor(
     private sharedService: SharedService,
-    private routerparams: Router) {
+    private routerparams: Router,
+    private activatedRoute: ActivatedRoute) {
     this.sharedService.setTimeLineCobertura(true);
     this.sharedService.setTimeLineDatosPersonales(true);
     this.sharedService.setTimeLineBiometriaFacial(true);
@@ -31,29 +35,60 @@ export class BiometriaFacialComponent implements OnInit {
     this.sharedService.setTimeLineActivo1(false);
     this.sharedService.setTimeLineActivo2(false);
     this.sharedService.setTimeLineActivo4(false);
+
+    const fingerCodeUuid = localStorage.getItem('finger_code_uuid') || '';
+    const urlBiometria = localStorage.getItem('url_biometria') || '';
+    const urlRedirect = window.location.href + '&uid='; //localStorage.getItem('url_redirect') || '';
+
+    if (fingerCodeUuid && urlBiometria && urlRedirect) {
+      this.urlValidacionBiometriaFacial = urlBiometria + urlRedirect + fingerCodeUuid;
+    }
+
+    this.verificarRespuestaValidacionBiometrica();
   }
 
   ngOnInit(): void {
   }
 
   solicitarValidacion() {
-    console.log('solicitar validacion');
-    this.panelSolicitarValidacion = false;
-    this.panelValidaIdentidad = true;
-    this.panelVerificaIdentidad = true;
-    // logica para habilitar boton una vez que se ha verificado por whatsapp o correo
-    setTimeout(() => {
-      this.panelVerificaIdentidad = false;
-      this.panelValidacionExitosa = true;
-      this.showValidacionExistosa();
-      this.validacionExistosa = true;
-    }, 3000);
+    console.log('solicitar validacion ', this.urlValidacionBiometriaFacial);
+
+    if (this.urlValidacionBiometriaFacial) {
+      window.location.href = this.urlValidacionBiometriaFacial;
+    }
+
+
+    // this.panelSolicitarValidacion = false;
+    // this.panelValidaIdentidad = true;
+    // this.panelVerificaIdentidad = true;
+    // // logica para habilitar boton una vez que se ha verificado por whatsapp o correo
+    // setTimeout(() => {
+    //   this.panelVerificaIdentidad = false;
+    //   this.panelValidacionExitosa = true;
+    //   this.showValidacionExistosa();
+    //   this.validacionExistosa = true;
+    // }, 3000);
   }
 
-  showValidacionExistosa() {
+  verificarRespuestaValidacionBiometrica() {
+    this.respuestaBiometria = (this.activatedRoute.snapshot.queryParams['_Response']) ? JSON.parse(this.activatedRoute.snapshot.queryParams['_Response']) : undefined;
+    console.log('params ', this.respuestaBiometria);
+
+    if (this.respuestaBiometria) {
+      const estado = (this.respuestaBiometria.Extras.StateName !== 'Error') ? true : false;
+      // mostrar validacion exitosa o fallida
+      this.panelSolicitarValidacion = false;
+      this.panelValidaIdentidad = true;
+      this.panelValidacionExitosa = true;
+      this.showValidacionExistosa(estado);
+      this.validacionExistosa = estado;
+    }
+  }
+
+  showValidacionExistosa(estado: boolean) {
     this.spinner = true;
     setTimeout(() => {
-      this.showIconValidacionExistosa = true;
+      this.showIconValidacionExistosa = estado;
       this.spinner = false;
     }, 500);
   }
