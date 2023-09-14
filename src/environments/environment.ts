8ca1acea58b0089ea9f9f67849934594f780b940
HenryGye <henryedua@hotmail.com>
2023-09-14 13:35:39 -0500

    Agregar environment de prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  API_KEY: 'rihocfpqecocfuipicojqieorvpenc90jer9vuceoirvhsadic',
+  MAIN_URL: 'https://autoservicio.xtrim.com.ec/',
+  API_CONSULTAR_COBERTURA: 'backend/api/v1/FindNodeByCoordinates',
+  API_CONSULTAR_CALLE_PRINCIPAL_SECUNDARIA: 'backend/api/v1/ConsultaDirDicTitan',
+  API_CONSULTAR_TOKEN_CODIGO_DACTILAR: 'backend/api/v1/check',
+  API_CONSULTAR_BURO_CLIENTE: 'backend/api/v1/findByIdentificationNumber',
+  API_CONSULTAR_DATOS_CLIENTE: 'backend/api/v1/dniAccounts',
+  API_ACEPTACION_CONTRATO: 'backend/api/v1/accept',
 };
